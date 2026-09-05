a18843f8739a19363d24ee974fcdf15ebd81a5e6
Sujal-Mishra <sujalmishra.0306@gmail.com>
2026-09-05 22:19:03 +0530

    chore: remove pacman directory from color-memory branch

--- pacman/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Map } from "@/components/Map";
-
-export default function Home() {
-  return (
-    <div className="flex flex-1 items-center justify-center bg-black p-4">
-      <Map />
-    </div>
-  );
-}
